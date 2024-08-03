7c1e02a8ee58bdd2d5ab6cfd00911e7653c99413
loezix <loezix@proton.me>
2024-08-02 22:21:58 -0300

    +jacoco

--- buildSrc/src/main/kotlin/commons.gradle.kts
+++ buildSrc/src/main/kotlin/commons.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("java")
   id("java-test-fixtures")
   id("maven-publish")
+  id("jacoco")
 }
 
 group = providers.gradleProperty("project.group").get()
@@ -24,5 +25,14 @@ java {
 
 tasks.withType<Test> {
   useJUnitPlatform()
+  finalizedBy(tasks.jacocoTestReport)
   systemProperty("cucumber.junit-platform.naming-strategy", "long")
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
+  reports {
+    xml.required = true
+    html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+  }
 }
