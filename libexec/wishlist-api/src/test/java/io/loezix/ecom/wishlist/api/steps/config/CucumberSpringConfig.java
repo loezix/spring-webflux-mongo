03cbbe4a23241fd24bba1e4627b309d763fddeed
loezix <loezix@proton.me>
2024-08-03 02:19:19 -0300

    done - jacoco

--- libexec/wishlist-api/src/test/java/io/loezix/ecom/wishlist/api/steps/config/CucumberSpringConfig.java
+++ libexec/wishlist-api/src/test/java/io/loezix/ecom/wishlist/api/steps/config/CucumberSpringConfig.java
@@ -2,9 +2,15 @@ package io.loezix.ecom.wishlist.api.steps.config;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest
 public class CucumberSpringConfig {
 
+  @TestConfiguration
+  static class SpringTest {}
 }
